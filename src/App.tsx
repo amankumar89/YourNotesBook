8da189e3dc2ee350dc3ec98297d2d1147e40e7c1
amankumar89 <amankumaroo784@gmail.com>
2023-11-22 17:20:32 +0530

    added header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import CourseGoal from "./components/CourseGoal";
+import goalsImage from './assets/goals.jpg';
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <main>
+      <Header
+        image={{
+          src: goalsImage,
+          alt: 'A list of goals'
+        }}
+      >
+        <h1>Your Course Goals</h1>
+      </Header>
       <CourseGoal
         title="Learn React + TS"
       >
---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+import { type PropsWithChildren } from "react";
+
+type ImageProps = {
+  src: string;
+  alt: string;
+}
+
+type HeaderProps = PropsWithChildren<{ image: ImageProps }>
+
+export default function Header(
+  { image, children }: HeaderProps,
+) {
+  return (
+    <header>
+      {/* <img src={image.src} alt={image.src} /> */}
+      <img {...image} />
+      {children}
+    </header>
+  )
+};
